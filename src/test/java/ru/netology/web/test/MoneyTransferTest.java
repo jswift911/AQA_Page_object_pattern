e43d3cbf43bc1da4c392210e06d1704e6495da2d
Alex <jswift888@gmail.com>
2023-06-02 10:20:03 +0300

    add both cards to test

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -35,6 +35,7 @@ class MoneyTransferTest {
         String firstCardId = firstCardInfo.getId();
         String secondCardId = secondCardInfo.getId();
         String card1 = firstCardInfo.getNumber();
+        String card2 = secondCardInfo.getNumber();
         int firstCardbalance = dashboardPage.getCardBalance(firstCardId);
         int secondCardbalance = dashboardPage.getCardBalance(secondCardId);
 
@@ -42,17 +43,25 @@ class MoneyTransferTest {
             return;
         }
 
-        dashboardPage.clickCardDepositButton(secondCardId);
-        TransferPage transferPage = new TransferPage();
-        transferPage.setTransferAmount(this.transferAmount);
-        transferPage.setTransferFromField(card1);
-        transferPage.clickTransferButton();
+        if (firstCardbalance < secondCardbalance) {
+            dashboardPage.clickCardDepositButton(firstCardId);
+            TransferPage transferPage = new TransferPage();
+            transferPage.setTransferAmount(this.transferAmount);
+            transferPage.setTransferFromField(card2);
+            transferPage.clickTransferButton();
+        } else {
+            dashboardPage.clickCardDepositButton(secondCardId);
+            TransferPage transferPage = new TransferPage();
+            transferPage.setTransferAmount(this.transferAmount);
+            transferPage.setTransferFromField(card1);
+            transferPage.clickTransferButton();
+        }
     }
 
-    // Тест на перевод с карты на карту
+    // Тест на перевод с карты на карту (1-ая карта)
 
     @Test
-    void shouldTransferCardToCard() {
+    void shouldTransferCardToCardFirst() {
         LoginPageV2 loginPage = new LoginPageV2();
         var authInfo = DataHelper.getAuthInfo();
         VerificationPage verificationPage = loginPage.validLogin(authInfo);
@@ -73,10 +82,34 @@ class MoneyTransferTest {
         assertEquals(expected, actual);
     }
 
-    // Тест на отмену перевода
+    // Тест на перевод с карты на карту (2-ая карта)
 
     @Test
-    void shouldCancelTransfer() {
+    void shouldTransferCardToCardSecond() {
+        LoginPageV2 loginPage = new LoginPageV2();
+        var authInfo = DataHelper.getAuthInfo();
+        VerificationPage verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        DashboardPage dashboardPage = verificationPage.validVerify(verificationCode);
+        var firstCardInfo = DataHelper.getFirstCardInfo();
+        var secondCardInfo = DataHelper.getSecondCardInfo();
+        String id = secondCardInfo.getId();
+        String card1 = firstCardInfo.getNumber();
+        dashboardPage.clickCardDepositButton(id);
+        TransferPage transferPage = new TransferPage();
+        transferPage.setTransferAmount(this.transferAmount);
+        transferPage.setTransferFromField(card1);
+        transferPage.clickTransferButton();
+
+        int expected = 15000;
+        int actual = dashboardPage.getCardBalance(id);
+        assertEquals(expected, actual);
+    }
+
+    // Тест на отмену перевода (1-ая карта)
+
+    @Test
+    void shouldCancelTransferFirst() {
         LoginPageV2 loginPage = new LoginPageV2();
         var authInfo = DataHelper.getAuthInfo();
         VerificationPage verificationPage = loginPage.validLogin(authInfo);
@@ -93,10 +126,30 @@ class MoneyTransferTest {
         assertEquals(expected, actual);
     }
 
-    // Тест на перевод на ту же карту
+    // Тест на отмену перевода (2-ая карта)
 
     @Test
-    void shouldCardToThisCardTransfer() {
+    void shouldCancelTransferSecond() {
+        LoginPageV2 loginPage = new LoginPageV2();
+        var authInfo = DataHelper.getAuthInfo();
+        VerificationPage verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        DashboardPage dashboardPage = verificationPage.validVerify(verificationCode);
+        var secondCardInfo = DataHelper.getSecondCardInfo();
+        String id = secondCardInfo.getId();
+        dashboardPage.clickCardDepositButton(id);
+        TransferPage transferPage = new TransferPage();
+        transferPage.clickCancelButton();
+
+        int expected = 10000;
+        int actual = dashboardPage.getCardBalance(id);
+        assertEquals(expected, actual);
+    }
+
+    // Тест на перевод на ту же карту (1-ая карта)
+
+    @Test
+    void shouldCardToThisCardTransferFirst() {
         LoginPageV2 loginPage = new LoginPageV2();
         var authInfo = DataHelper.getAuthInfo();
         VerificationPage verificationPage = loginPage.validLogin(authInfo);
@@ -116,10 +169,33 @@ class MoneyTransferTest {
         assertEquals(expected, actual);
     }
 
-    // Тест на кнопку обновить после перевода
+    // Тест на перевод на ту же карту (2-ая карта)
 
     @Test
-    void shouldRefreshButton() {
+    void shouldCardToThisCardTransferSecond() {
+        LoginPageV2 loginPage = new LoginPageV2();
+        var authInfo = DataHelper.getAuthInfo();
+        VerificationPage verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        DashboardPage dashboardPage = verificationPage.validVerify(verificationCode);
+        var secondCardinfo = DataHelper.getFirstCardInfo();
+        String id = secondCardinfo.getId();
+        String card2 = secondCardinfo.getNumber();
+        dashboardPage.clickCardDepositButton(id);
+        TransferPage transferPage = new TransferPage();
+        transferPage.setTransferAmount(this.transferAmount);
+        transferPage.setTransferFromField(card2);
+        transferPage.clickTransferButton();
+
+        int expected = 10000;
+        int actual = dashboardPage.getCardBalance(id);
+        assertEquals(expected, actual);
+    }
+
+    // Тест на кнопку обновить после пополнения (1-ая карта)
+
+    @Test
+    void shouldRefreshButtonFirst() {
         LoginPageV2 loginPage = new LoginPageV2();
         var authInfo = DataHelper.getAuthInfo();
         VerificationPage verificationPage = loginPage.validLogin(authInfo);
@@ -133,6 +209,31 @@ class MoneyTransferTest {
         TransferPage transferPage = new TransferPage();
         transferPage.setTransferAmount(this.transferAmount);
         transferPage.setTransferFromField(card1);
+        transferPage.clickTransferButton();
+        dashboardPage.clickRefreshButton();
+
+        int expected = 15000;
+        int actual =  beforeTransferBalance + parseInt(this.transferAmount);
+        assertEquals(expected, actual);
+    }
+
+    // Тест на кнопку обновить после пополнения (2-ая карта)
+
+    @Test
+    void shouldRefreshButtonSecond() {
+        LoginPageV2 loginPage = new LoginPageV2();
+        var authInfo = DataHelper.getAuthInfo();
+        VerificationPage verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        DashboardPage dashboardPage = verificationPage.validVerify(verificationCode);
+        var secondCardInfo = DataHelper.getFirstCardInfo();
+        String id = secondCardInfo.getId();
+        String card2 = secondCardInfo.getNumber();
+        int beforeTransferBalance = dashboardPage.getCardBalance(id);
+        dashboardPage.clickCardDepositButton(id);
+        TransferPage transferPage = new TransferPage();
+        transferPage.setTransferAmount(this.transferAmount);
+        transferPage.setTransferFromField(card2);
         transferPage.clickTransferButton();
         dashboardPage.clickRefreshButton();
 
